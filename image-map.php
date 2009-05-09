34a5f4b1d60f9dd22fa2c5d5c225a070bf905daf
Joey Degges <jdegges@gmail.com>
2009-05-09 11:39:13 -0700

    update

--- image-map.php
+++ image-map.php
@@ -1,22 +1,78 @@
-<DIV ALIGN=CENTER>
+<?php
 
-<MAP NAME="map1">
-<AREA
-   HREF="contacts.html" ALT="Contacts" TITLE="Contacts"
-   SHAPE=CIRCLE COORDS="357, 285, 8">
-<AREA
-   HREF="products.html" ALT="Products" TITLE="Products"
-   SHAPE=CIRCLE COORDS="372, 356, 8">
-<AREA
-   HREF="new.html" ALT="New!" TITLE="New!"
-   SHAPE=CIRCLE COORDS="423, 502, 8">
-</MAP>
+$width  = 1500;
+$height = 1500;
+$scale  = 1;
+$radius = 8*$scale;
 
-<IMG SRC="locations_map_lg.jpg"
-   ALT="map of GH site" BORDER=0 WIDTH=1500 HEIGHT=1500
-   USEMAP="#map1"><BR>
+/* print header */
+function echo_header() {
+    echo "<DIV ALIGN=CENTER>\n";
+    echo "\t<MAP NAME=\"image-map\">\n";
+}
 
-[ <A HREF="contacts.html" ALT="Contacts">Contacts</A> ]
-[ <A HREF="products.html" ALT="Products">Products</A> ]
-[ <A HREF="new.html"      ALT="New!">New!</A> ]
-</DIV>
+/* print footer */
+function echo_footer() {
+    global $width;
+    global $height;
+    global $scale;
+
+    echo "\t</MAP>\n";
+    echo "\t<IMG SRC=\"locations_map_lg.jpg\" ALT=\"Locations Map\" BORDER=0 ";
+    echo "WIDTH=".$width*$scale." HEIGHT=".$height*$scale;
+    echo " USEMAP=\"#image-map\">\n";
+    echo "</DIV>\n";
+}
+
+/* print each location */
+function echo_locations($locs) {
+    global $scale;
+    global $radius;
+    foreach ($locs as $loc) {
+        echo "\t\t<AREA HREF=\"image-map.php?loc=$loc[0]\" ALT=\"$loc[0]\" ";
+        echo "TITLE=\"$loc[0]\" SHAPE=CIRCLE ";
+        echo "COORDS=\"".$loc[1]*$scale.", ".$loc[2]*$scale.", $radius\">\n";
+    }
+}
+
+/* read locations from the file $loc_file
+ * each line of the file contains data for one point
+ * there are three fields on each line each separated by a comma:
+ * first  -> location identifier
+ * second -> x coordinate of the center of the circle
+ * thrid  -> y coordinate of the center of the circle
+*/
+function get_locations($loc_file) {
+    $locs = array();
+    $fh = fopen($loc_file, "rb");
+
+    /* while there is data in the file */
+    while(!feof($fh)) {
+        /* grab a line from the file */
+        $line = fgets($fh);
+
+        /* if the line starts with a '#' or isnt long enough then ignore it */
+        if($line[0] == '#' || strlen($line) < 5)
+            continue;
+
+        /* convert the line into an array */
+        $pts = explode(',', $line);
+
+        /* chop off any whitespace from each field */
+        for($i = 0; $i < sizeof($pts); $i++) {
+            $pts[$i] = rtrim($pts[$i]);
+        }
+
+        /* push new data onto the end of the array */
+        array_push($locs, $pts);
+    }
+    return $locs;
+}
+
+
+echo_header();
+$pts = get_locations("locations.txt");
+echo_locations($pts);
+echo_footer();
+
+?>
